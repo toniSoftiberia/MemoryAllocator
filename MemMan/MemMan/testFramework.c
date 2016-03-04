aada4241da42bca266186f70626d3ed202b5e961
toniSoftiberia <alopezs1987+social@gmail.com>
2016-03-04 13:25:35 +0100

    Uncommited Changes
    *added uncommited changes

---
+++ MemMan/MemMan/testFramework.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "testFramework.h"
+#include "Globals.h"
+
+int num_of_tests = 100;
+
+int callTest(UNITTEST *test)
+{
+	return (*test).test();
+};
+
+int callTestSuite(UNITTESTSUITE suite)
+{
+	int rc = 1;
+	UNITTEST* test = suite.tests;
+
+	if (suite.setup)
+		(suite.setup());
+
+	for (int i = 0; i < num_of_tests; ++i){
+		test = suite.tests;
+		while (test->test != 0)
+		{
+			if (callTest(test++))
+			{
+				printf(".");
+			}
+			else
+			{
+				printf("#");
+				rc = 0;
+			}
+		}
+	}
+
+	if (suite.teardown)
+		(suite.teardown());
+
+	return rc;
+}
+
+void testFailed(const char* testName, const int testLine, const char* message)
+{
+	fprintf(stderr, "%s(%i): ERROR test failed, %s\n", testName, testLine, message);
+	OWN_LOG("%s(%i): ERROR test failed, %s\n", testName, testLine, message);
+};
