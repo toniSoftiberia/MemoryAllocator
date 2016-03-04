be4b3577da7c8e7ceff42523b896586ae564f707
toniSoftiberia <alopezs1987+social@gmail.com>
2016-03-04 14:48:04 +0100

    Refactor
    Refactoring

--- MemMan/MemMan/MemMan.h
+++ MemMan/MemMan/MemMan.h
@@ -14,17 +14,16 @@ struct Pool {
 Pool* my_pool;
 
 extern char* string_strip;
-//extern int num_of_test = 5;
 
-extern int getFreeMemoryBlocks();
-extern int markMap(int pos, int lenght, char free);
-extern int initMap();
-extern int initializePool(int pool_size);
-extern void deletePool();
 extern void* _malloc(int lenght);
 extern int _free(void* ptr);
+extern int initializePool(int pool_size);
+extern int initMap();
+extern void deletePool();
+extern int isFree(int pos);
+extern int getFreeMemoryBlocks();
+extern int markMap(int pos, int lenght, char free);
 extern int getPos(void * ptr);
 extern void DumpDetailedPool();
 extern void DumpPool();
-extern int isFree(int pos);
 
